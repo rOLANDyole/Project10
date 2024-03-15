a1fe0012dd5b41888a523793c7e84649eb6b308b
yaroslav.GOMZ.R.Y.A.W <157982040+rOLANDyole@users.noreply.github.com>
2024-03-15 17:40:21 +0500

    Update main.cpp

--- Project10/main.cpp
+++ Project10/main.cpp
@@ -12,39 +12,39 @@ int main()
 		{
 			cout << "rub";
 		}
-		if (number / 1000 == 1)
+		else if (number / 1000 == 1)
 		{
 			cout << "one thousand";
 		}
-		if (number / 1000 == 2)
+		else if (number / 1000 == 2)
 		{
 			cout << "two thousand";
 		}
-		if (number / 1000 == 3)
+		else if (number / 1000 == 3)
 		{
 			cout << "three thousand";
 		}
-		if (number / 1000 == 4)
+		else if (number / 1000 == 4)
 		{
 			cout << "four thousand";
 		}
-		if (number / 1000 == 5)
+		else if (number / 1000 == 5)
 		{
 			cout << "five thousand";
 		}
-		if (number / 1000 == 6)
+		else if (number / 1000 == 6)
 		{
 			cout << "six thousand";
 		}
-		if (number / 1000 == 7)
+		else if (number / 1000 == 7)
 		{
 			cout << "seven thousand";
 		}
-		if (number / 1000 == 8)
+		else if (number / 1000 == 8)
 		{
 			cout << "eight thousand";
 		}
-		if (number / 1000 == 9)
+		else if (number / 1000 == 9)
 		{
 			cout << "nine thousand";
 		}
@@ -54,43 +54,129 @@ int main()
 	{
 		if (number / 100 == 0)
 		{
-			cout << "no hundrends";
+			cout << "hundrend";
 		}
-		if (number / 100 == 1)
+		else if (number / 100 == 1)
 		{
-			cout << "one hundrends";
+			cout << "one hundrend";
 		}
-		if (number / 100 == 2)
+		else if (number / 100 == 2)
 		{
-			cout << "two hundrends";
+			cout << "two hundrend";
 		}
-		if (number / 100 == 3)
+		else if (number / 100 == 3)
 		{
-			cout << "three hundrends";
+			cout << "three hundrend";
 		}
-		if (number / 100 == 4)
+		else if (number / 100 == 4)
 		{
-			cout << "four hundrends";
+			cout << "four hundrend";
 		}
-		if (number / 100 == 5)
+		else if (number / 100 == 5)
 		{
-			cout << "five hundrends";
+			cout << "five hundrend";
 		}
-		if (number / 100 == 6)
+		else if (number / 100 == 6)
 		{
-			cout << "six hundrends";
+			cout << "six hundrend";
 		}
-		if (number / 100 == 7)
+		else if (number / 100 == 7)
 		{
-			cout << "seven hundrends";
+			cout << "seven hundrend";
 		}
-		if (number / 100 == 8)
+		else if (number / 100 == 8)
 		{
-			cout << "eight hundrends";
+			cout << "eight hundrend";
 		}
-		if (number / 100 == 9)
+		else if (number / 100 == 9)
 		{
-			cout << "nine hundrends";
+			cout << "nine hundrend";
+		}
+	}
+	if ((number / 10) % 10 != 1)
+	{
+		if (number / 10 == 0)
+		{
+			cout << "hundrend";
+		}
+		else if (number / 10 == 1)
+		{
+			cout << "eleven";
+		}
+		else if (number / 10 == 2)
+		{
+			cout << "twelve";
+		}
+		else if (number / 10 == 3)
+		{
+			cout << "thretenn";
+		}
+		else if (number / 10 == 4)
+		{
+			cout << "fourteen";
+		}
+		else if (number / 10 == 5)
+		{
+			cout << "fifteen";
+		}
+		else if (number / 10 == 6)
+		{
+			cout << "sixteen";
+		}
+		else if (number / 10 == 7)
+		{
+			cout << "seventeen";
+		}
+		else if (number / 10 == 8)
+		{
+			cout << "eighteen";
+		}
+		else if (number / 10 == 9)
+		{
+			cout << "nineteen";
+		}
+	}
+	if ((number / 100) % 100 != 1)
+	{
+		if (number % 10 == 0)
+		{
+			cout << "zero";
+		}
+		else if (number % 10 == 1)
+		{
+			cout << "one";
+		}
+		else if (number % 10 == 2)
+		{
+			cout << "two";
+		}
+		else if (number % 10 == 3)
+		{
+			cout << "three";
+		}
+		else if (number % 10 == 4)
+		{
+			cout << "four";
+		}
+		else if (number % 10 == 5)
+		{
+			cout << "five";
+		}
+		else if (number % 10 == 6)
+		{
+			cout << "six";
+		}
+		else if (number % 10 == 7)
+		{
+			cout << "seven";
+		}
+		else if (number % 10 == 8)
+		{
+			cout << "eight";
+		}
+		else if (number % 10 == 9)
+		{
+			cout << "nine";
 		}
 	}
 }
